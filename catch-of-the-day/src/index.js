3336e7636a4f6a16958343b3b20b11e60eea6cc7
Laura Ayala <ayala.lauramichelle@gmail.com>
2016-12-25 16:58:18 -0500

    Add routes

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 // will load info from react library into React variable to be used throughout the application
 import { render } from 'react-dom';
 // we only want the render method from the ReactDOM package
+
+import { BrowserRouter, Match, Miss } from 'react-router';
+
 import './css/style.css';
 // importing our custom stylesheet, which webpack will compile for us and add a style tag into the html
 import App from './components/App';
@@ -9,6 +12,20 @@ import App from './components/App';
 import StorePicker from './components/StorePicker';
 // have to refer to StorePicker using a regular path so that React will recognize it as such and not think it's an npm package
 // .js not necessary; it's assumed
+import NotFound from './components/NotFound';
 
-render(<App/>, document.querySelector('#main'));
+const Root = () => {
+  return (
+      <BrowserRouter>
+        <div>
+          <Match exactly pattern="/" component={ StorePicker } />
+          <Match exactly pattern="/store/:storeId" component={ App } />
+          <Miss component={ NotFound} />
+        </div>
+      </BrowserRouter>
+    )
+}
+
+render(<Root/>, document.querySelector('#main'));
 // will render the StorePicker component to the page, where we told it to (div with id of main)
+
