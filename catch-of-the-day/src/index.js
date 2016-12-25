d7fee3d1113d60906d597088589cc8e7dc926c91
Laura Ayala <ayala.lauramichelle@gmail.com>
2016-12-25 16:13:51 -0500

    Add stylesheet

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 // will load info from react library into React variable to be used throughout the application
 import { render } from 'react-dom';
 // we only want the render method from the ReactDOM package
-
+import './css/style.css';
+// importing our custom stylesheet, which webpack will compile for us and add a style tag into the html
 import StorePicker from './components/StorePicker';
 // have to refer to StorePicker using a regular path so that React will recognize it as such and not think it's an npm package
 // .js not necessary; it's assumed
