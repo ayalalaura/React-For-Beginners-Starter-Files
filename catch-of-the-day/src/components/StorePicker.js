ab1c846a8d4ad70ec5348cd620900f973e413750
Laura Ayala <ayala.lauramichelle@gmail.com>
2016-12-25 15:40:51 -0500

    Add StorePicker component

---
+++ catch-of-the-day/src/components/StorePicker.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// goes at the top of every component file
+
+class StorePicker extends React.Component {
+  render() {
+    return <p>Hello</p>
+  }
+  // every component needs at least one method - the render()
+}
+
+export default StorePicker;
--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,2 +1,11 @@
-// let's go!
-// alert("yeah!");
+import React from 'react';
+// will load info from react library into React variable to be used throughout the application
+import { render } from 'react-dom';
+// we only want the render method from the ReactDOM package
+
+import StorePicker from './components/StorePicker';
+// have to refer to StorePicker using a regular path so that React will recognize it as such and not think it's an npm package
+// .js not necessary; it's assumed
+
+render(<StorePicker/>, document.querySelector('#main'));
+// will render the StorePicker component to the page, where we told it to (div with id of main)
